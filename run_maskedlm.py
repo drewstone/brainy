6a39cafc6f9294c80526a676736624e122322113
Drew Stone <drewstone329@gmail.com>
2020-03-07 18:39:33 -0700

    Push example

---
+++ run_maskedlm.py
@@ -0,0 +1,31 @@
+import torch
+from transformers import *
+
+# Each architecture is provided with several class for fine-tuning on down-stream tasks, e.g.
+BERT_MODEL_CLASSES = [BertForMaskedLM]
+
+# All the classes for an architecture can be initiated from pretrained weights for this architecture
+# Note that additional weights added for fine-tuning are only initialized
+# and need to be trained on the down-stream task
+pretrained_weights = 'bert-base-uncased'
+tokenizer = BertTokenizer.from_pretrained(pretrained_weights)
+for model_class in BERT_MODEL_CLASSES:
+    # Load pretrained model/tokenizer
+    model = model_class.from_pretrained(pretrained_weights)
+
+    # Models can return full list of hidden-states & attentions weights at each layer
+    model = model_class.from_pretrained(pretrained_weights,
+                                        output_hidden_states=True,
+                                        output_attentions=True)
+    input_ids = torch.tensor([tokenizer.encode("Let's see all hidden-states and attentions on this text")])
+    all_hidden_states, all_attentions = model(input_ids)[-2:]
+
+    # Models are compatible with Torchscript
+    model = model_class.from_pretrained(pretrained_weights, torchscript=True)
+    traced_model = torch.jit.trace(model, (input_ids,))
+
+    # Simple serialization for models and tokenizers
+    model.save_pretrained('./bert/')  # save
+    model = model_class.from_pretrained('./bert/')  # re-load
+    tokenizer.save_pretrained('./bert/')  # save
+    tokenizer = BertTokenizer.from_pretrained('./bert/')  # re-load
